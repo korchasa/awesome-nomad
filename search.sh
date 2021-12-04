435e8a0351db50587369ec141e2e23ed8ada60d5
korchasa <korchasa@icloud.com>
2021-12-04 14:45:13 +0200

    fix: sed: RE error: illegal byte sequence

--- search.sh
+++ search.sh
@@ -2,7 +2,7 @@
 date=$(date -v -3m '+%Y-%m-%d')
 resp=$(http https://api.github.com/search/repositories\?q\=nomad+archived:false+pushed:\>$date\&sort\=stars\&order\=desc\&per_page\=100 | jq -r '.items[] | select(.stargazers_count > 30) | "- [" + .full_name + "](" + .html_url + ") - " + .description')
 while read -r item; do
-    id=$(echo ${item} | sed 's/.*\[\([^)]*\)\].*/\1/')
+    id=$(echo ${item} | LC_ALL=C sed 's/.*\[\([^)]*\)\].*/\1/')
     if ! grep -q "${id}" ./README.md; then
         if ! grep -q "${id}" ./ignore; then
             echo ${item}
